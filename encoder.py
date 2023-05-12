2e6ee033a05773fcfaa8e91eff89e13bff86aa23
samjwu <sjwu@ualberta.ca>
2023-05-11 21:34:12 -0600

    formatting

--- encoder.py
+++ encoder.py
@@ -8,11 +8,19 @@ import torch.nn
 
 
 class Encoder(torch.nn.Module):
-    def __init__(self, hidden_size: int, embedding: torch.nn.Embedding, num_layers: int=1, dropout: int=0) -> None:
+    def __init__(
+        self, 
+        hidden_size: int, 
+        embedding: torch.nn.Embedding, 
+        num_layers: int=1, 
+        dropout: int=0
+    ) -> None:
         super(Encoder, self).__init__()
-        self.num_layers = num_layers
+
         self.hidden_size = hidden_size
         self.embedding = embedding
+        self.num_layers = num_layers
+        
         self.gru = torch.nn.GRU(
             input_size=hidden_size,
             hidden_size=hidden_size,
