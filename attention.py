f2710f8127e0a285fdf7e644f16335a8c6b33c7e
samjwu <sjwu@ualberta.ca>
2023-05-14 18:26:56 -0600

    add score functions

--- attention.py
+++ attention.py
@@ -3,6 +3,7 @@ Attention is to enhance certain parts of the input sequence and diminish other p
 Calculated using output vectors from the encoder and hidden state from the decoder.
 """
 
+import torch
 import torch.nn
 
 
@@ -21,3 +22,16 @@ class Attention(nn.Module):
         elif self.method == 'concat':
             self.attention = torch.nn.Linear(self.hidden_size * 2, hidden_size)
             self.parameter = torch.nn.Parameter(torch.FloatTensor(hidden_size))
+
+    def calculate_dot_score(self, hidden_state_vectors, encoder_output) -> Tensor:
+        return torch.sum(hidden_state_vectors * encoder_output, dim=2)
+
+    def calculate_general_score(self, hidden_state_vectors, encoder_output) -> Tensor:
+        energy = self.attention(encoder_output)
+        return torch.sum(hidden_state_vectors * energy, dim=2)
+
+    def calculate_concat_score(self, hidden_state_vectors, encoder_output) -> Tensor:
+        energy = self.attention(torch.cat(
+            (hidden_state_vectors.expand(encoder_output.size(0), -1, -1), encoder_output),
+            2)).tanh()
+        return torch.sum(self.parameter * energy, dim=2)
