63305460ee540665c5f58e2023db43235058d4c8
samjwu <sjwu@ualberta.ca>
2023-05-16 20:27:26 -0600

    forward pass for attention

--- attention.py
+++ attention.py
@@ -5,6 +5,7 @@ Calculated using output vectors from the encoder and hidden state from the decod
 
 import torch
 import torch.nn
+import torch.nn.functional
 
 
 class Attention(nn.Module):
@@ -23,21 +24,37 @@ class Attention(nn.Module):
             self.attention = torch.nn.Linear(self.hidden_size * 2, hidden_size)
             self.parameter = torch.nn.Parameter(torch.FloatTensor(hidden_size))
 
-    def calculate_dot_score(self, hidden_state_vectors, encoder_output) -> Tensor:
-        return torch.sum(hidden_state_vectors * encoder_output, dim=2)
+    def calculate_dot_score(self, hidden_state_vectors, encoder_output_vectors) -> Tensor:
+        return torch.sum(hidden_state_vectors * encoder_output_vectors, dim=2)
 
-    def calculate_general_score(self, hidden_state_vectors, encoder_output) -> Tensor:
-        energy = self.attention(encoder_output)
+    def calculate_general_score(self, hidden_state_vectors, encoder_output_vectors) -> Tensor:
+        energy = self.attention(encoder_output_vectors)
         return torch.sum(hidden_state_vectors * energy, dim=2)
 
-    def calculate_concat_score(self, hidden_state_vectors, encoder_output) -> Tensor:
+    def calculate_concat_score(self, hidden_state_vectors, encoder_output_vectors) -> Tensor:
         energy = self.attention(
             torch.cat(
                 (
-                    hidden_state_vectors.expand(encoder_output.size(0), -1, -1),
-                    encoder_output,
+                    hidden_state_vectors.expand(encoder_output_vectors.size(0), -1, -1),
+                    encoder_output_vectors,
                 ),
                 2,
             )
         ).tanh()
         return torch.sum(self.parameter * energy, dim=2)
+
+    def forward(self, hidden_state_vectors, encoder_output_vectors) -> Tensor:
+        """
+        Calculate attention weights or energies
+        and return the normalized probability distribution of the outputs (softmax).
+        """
+        if self.method == 'general':
+            attention_weights = self.general_score(hidden_state_vectors, encoder_output_vectors)
+        elif self.method == 'concat':
+            attention_weights = self.concat_score(hidden_state_vectors, encoder_output_vectors)
+        elif self.method == 'dot':
+            attention_weights = self.dot_score(hidden_state_vectors, encoder_output_vectors)
+
+        attention_weights = attention_weights.t()
+
+        return torch.nn.functional.softmax(attention_weights, dim=1).unsqueeze(1)
