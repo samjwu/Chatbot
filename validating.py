28e3378b7997bec88fbf9307de382e44d5a77d46
samjwu <sjwu@ualberta.ca>
2023-05-25 19:50:52 -0600

    add validation method

---
+++ validating.py
@@ -0,0 +1,24 @@
+"""Utility functions for validating an attention model."""
+
+import torch
+from encoder import Encoder
+from decoder import Decoder
+from vocabulary import Vocabulary
+from greedy_search import GreedySearch
+
+def validate_sentence(encoder: Encoder, decoder: Decoder, searcher: GreedySearch, vocab: Vocabulary, sentence: str, max_length: int=10) -> list[str]:
+    """Evaluate an input string/sentence and return a list/batch of decoded words."""
+    # convert the sentence into its indices and get their lengths
+    indices_batch = [sentence_to_indices(vocab, sentence)]
+    lengths = torch.tensor([len(indices) for indices in indices_batch])
+    
+    # transpose dimensions of input batch to match the attention model's expectations
+    input_batch = torch.LongTensor(indices_batch).transpose(0, 1)
+    
+    input_batch = input_batch.to(device)
+    lengths = lengths.to("cpu")
+
+    # greedily decode the sentence and return it as a batch
+    tokens, scores = searcher(input_batch, lengths, max_length)
+    decoded_words = [vocab.index_to_word[token.item()] for token in tokens]
+    return decoded_words
